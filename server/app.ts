37fdf75f9af653ddcfcf26f5c4787ec8d4ce550e
loxt <emannuelloxt@hotmail.com>
2020-04-29 13:48:22 -0400

    feat: crud completed

--- server/app.ts
+++ server/app.ts
@@ -10,9 +10,14 @@ class App {
 
   constructor() {
     this.app = express();
+
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+
     this.database = new database();
     this.database.createConnection();
     this.controller = new controller();
+
     this.routes();
   }
 
@@ -23,8 +28,14 @@ class App {
       }));
     this.app.route('/api/crushes').get(
       (req, res) => this.controller.select(req, res));
+    this.app.route('/api/crushes').post(
+      (req, res) => this.controller.insert(req, res));
     this.app.route('/api/crushes/:id').get(
       (req, res) => this.controller.selectOne(req, res));
+    this.app.route('/api/crushes/:id').delete(
+      (req, res) => this.controller.delete(req, res));
+    this.app.route('/api/crushes/:id').put(
+      (req, res) => this.controller.update(req, res));
   }
 }
 
--- server/controller.ts
+++ server/controller.ts
@@ -21,6 +21,37 @@ class Controller {
     this.getCrushesById(id).then(crushes => res.status(200).json(crushes))
       .catch(err => res.status(400).json(err))
   }
+
+  updateCrush(id, data) {
+    return model.findOneAndUpdate(id, data);
+  }
+
+  update(req, res) {
+    const id = { _id: req.params.id };
+    const crush = req.body;
+    this.updateCrush(id, crush).then(crushes => res.status(200).json(crushes))
+      .catch(err => res.status(400).json(err))
+  }
+
+  insertCrush(data) {
+    return model.create(data);
+  }
+
+  insert(req, res) {
+    const crush = req.body;
+    this.insertCrush(crush).then(crushes => res.status(200).json(crushes))
+      .catch(err => res.status(400).json(err))
+  }
+
+  deleteById(id) {
+    return model.deleteOne(id);
+  }
+
+  delete(req, res) {
+    const id = { _id: req.params.id }
+    this.deleteById(id).then(crushes => res.status(200).json(crushes))
+      .catch(err => res.status(400).json(err))
+  }
 }
 
 export default Controller;
